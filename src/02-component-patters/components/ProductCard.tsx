d2146e76d522bcc51beb1fa839bf3c91a3222332
juan.gomez <gomezj01@stepstone.com>
2022-04-05 09:58:17 -0600

    Compound component pattern - first step

--- src/02-component-patters/components/ProductCard.tsx
+++ src/02-component-patters/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import style from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg'
 import { useProduct } from '../hooks/useProduct';
+import { ReactElement } from 'react';
 
 interface Props {
   product: Product
@@ -12,20 +13,46 @@ interface Product {
   img?: string
 }
 
+interface ProductButtonsProps {
+  counter: number,
+  /* Differents ways to define a function on interface */
+  // increaseBy: Function
+  // increaseBy: () => void
+  increaseBy: (value: number) => void
+}
+
+export const ProductImage = ( { img = '' } ) => {
+  return (
+    <img src={ img ? img : noImage } alt="Coffee Mug" className={ style.productImg }/>
+  );
+}
+
+export const ProductTitle = ( { title }: { title: string } ) => {
+  return (
+    <span className={ style.productDescription }> { title } </span>
+  );
+};
+
+export const ProductButtons = ( { counter, increaseBy }: ProductButtonsProps ) => {
+  return (
+    <div className={ style.buttonsContainer }>
+      <button className={ style.buttonMinus } onClick= { () => increaseBy(-1) }> - </button>
+      <div className={ style.countLabel }> { counter } </div>
+      <button className={ style.buttonAdd } onClick= { () => increaseBy(1) }> + </button>
+    </div>
+  );
+}
+
 export const ProductCard = ( { product }: Props ) => {
   const { counter, increaseBy } = useProduct();
 
   return (
     <div className={ style.productCard }>
-      <img src={ product.img ? product.img : noImage } alt="Coffee Mug" className={ style.productImg }/>
-      {/* <img src={ noImage } alt="Coffee Mug" className={ style.productImg }/> */}
+      <ProductImage img={ product.img } />
 
-      <span className={ style.productDescription }> { product.title } </span>
-      <div className={ style.buttonsContainer }>
-        <button className={ style.buttonMinus } onClick= { () => increaseBy(-1) }> - </button>
-        <div className={ style.countLabel }> { counter } </div>
-        <button className={ style.buttonAdd } onClick= { () => increaseBy(1) }> + </button>
-      </div>
+      <ProductTitle title={ product.title } />
+
+      <ProductButtons counter={ counter } increaseBy= { increaseBy } />
     </div>
   )
 }
