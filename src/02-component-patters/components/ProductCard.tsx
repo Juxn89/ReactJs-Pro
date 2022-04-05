aafb694b1acaebb610ee28f693387d7b13faee64
juan.gomez <gomezj01@stepstone.com>
2022-04-05 08:38:08 -0600

    Send props to children component

--- src/02-component-patters/components/ProductCard.tsx
+++ src/02-component-patters/components/ProductCard.tsx
@@ -2,15 +2,25 @@ import style from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg'
 import { useProduct } from '../hooks/useProduct';
 
-export const ProductCard = () => {
+interface Props {
+  product: Product
+}
+
+interface Product {
+  id: string;
+  title: string;
+  img?: string
+}
+
+export const ProductCard = ( { product }: Props ) => {
   const { counter, increaseBy } = useProduct();
 
   return (
     <div className={ style.productCard }>
-      <img src="./coffee-mug.png" alt="Coffee Mug" className={ style.productImg }/>
+      <img src={ product.img ? product.img : noImage } alt="Coffee Mug" className={ style.productImg }/>
       {/* <img src={ noImage } alt="Coffee Mug" className={ style.productImg }/> */}
 
-      <span className={ style.productDescription }>Coffee Mug</span>
+      <span className={ style.productDescription }> { product.title } </span>
       <div className={ style.buttonsContainer }>
         <button className={ style.buttonMinus } onClick= { () => increaseBy(-1) }> - </button>
         <div className={ style.countLabel }> { counter } </div>
--- src/02-component-patters/pages/ShoppingPage.tsx
+++ src/02-component-patters/pages/ShoppingPage.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import { ProductCard } from '../components/ProductCard'
 
 export const ShoppingPage = () => {
+  const product = {
+    id: '1',
+    title: 'Coffee',
+    img: './coffee-mug.png'
+  }
+
   return (
     <div>
       <div>ShoppingPage</div>
       <hr/>
-      <ProductCard/>
+      <div style={ { display: 'flex', flexDirection: 'row', flexWrap:'wrap' } } >
+        <ProductCard product={ product }/>
+      </div>
     </div>
   )
 }
