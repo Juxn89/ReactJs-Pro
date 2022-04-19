e6161d1cabc09ed2fc9cebf805fb8b84bae1a9a3
juan.gomez <gomezj01@stepstone.com>
2022-04-19 10:22:59 -0600

    Prepare our components to deploy.

--- src/02-component-patters/pages/ShoppingPage.tsx
+++ src/02-component-patters/pages/ShoppingPage.tsx
@@ -1,6 +1,5 @@
 import { products } from '../data/products';
-import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components'
-import '../styles/custom-styles.css';
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
 
 const product = products[0];
 
@@ -9,18 +8,13 @@ export const ShoppingPage = () => {
     <div>
       <div>ShoppingPage</div>
       <hr/>
-      <ProductCard key={ product.id } product={ product } className='bg-dark text-white' initialValue={ { count: 4, maxCount: 10 } }>
+      <ProductCard key={ product.id } product={ product } initialValue={ { count: 4 } }>
         {
           ( { reset, isMaxCountReached, maxCount, increaseBy, count } ) => (
             <>
-              <ProductImage className='custom-image'/>
-              <ProductTitle className='text-bold'/>
-              <ProductButtons className='custom-buttons'/>
-
-              <button onClick={ reset }> Reset </button>
-              <button onClick={ () => increaseBy(-2) }>-2</button>
-              { (!isMaxCountReached && <button onClick={ () => increaseBy(2) } >+2</button> ) }
-              <span>{count} - { maxCount }</span>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
             </>
           )
         }
