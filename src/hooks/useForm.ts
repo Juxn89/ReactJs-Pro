956409fbf2a985d670e2f5e2369ebbf7baf40d61
juan.gomez <gomezj01@stepstone.com>
2022-06-09 09:14:04 -0600

    Extend functionality of useForm.

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -3,16 +3,27 @@ import { ChangeEvent, useState } from 'react';
 export const useForm = <T>(initialData: T) => {
   const [formData, setFormData] = useState( initialData );
 
-  const handleOnChange = (event:ChangeEvent<HTMLInputElement>) => {         
+  const handleOnChange = (event:ChangeEvent<HTMLInputElement>) => {
     const { target:{name} } = event;
     setFormData({
         ...formData,
-        [name]: event.target.value 
+        [name]: event.target.value
     });
  }
- 
+
+ const resetForm = () => {
+   setFormData( { ...initialData } );
+ }
+
+  const isValidEmail = ( email: string ) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
   return {
     formData,
+    resetForm,
+    isValidEmail,
     handleOnChange
   }
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import { useForm } from "../hooks/useForm";
 import '../styles/styles.css';
 
-const initialData = { 
+const initialData = {
     name: '',
     email: '',
     password: '',
@@ -10,7 +10,7 @@ const initialData = {
   }
 
 export const RegisterPage = () => {
-    const { formData, handleOnChange } = useForm(initialData);
+    const { formData, handleOnChange, resetForm, isValidEmail } = useForm(initialData);
 
      const { name, email, password, password2 } = formData;
 
@@ -23,11 +23,23 @@ export const RegisterPage = () => {
     <div>
         <h1>Register Page</h1>
         <form noValidate onSubmit={ handleOnSubmit }>
-            <input type="text" placeholder="Name" name="name" value={ name } onChange= { handleOnChange }/>
-            <input type="email" placeholder="Email" name="email" value={ email } onChange= { handleOnChange }/>
+            <input type="text" placeholder="Name" name="name" value={ name } onChange= { handleOnChange } className={ `${name.trim().length <= 0 && "has-error"}` }/>
+            { name.trim().length <= 0 && <span>This field is required</span> }
+
+            <input type="email" placeholder="Email" name="email" value={ email } onChange= { handleOnChange } className={ `${ !isValidEmail(email) && "has-error" }` }/>
+            { !isValidEmail(email) && <span>This field is required</span> }
+
             <input type="password" placeholder="Password" name="password" value={ password } onChange= { handleOnChange }/>
+            { password.trim().length <= 0 && <span>This field is required</span> }
+            { (password.trim().length > 0 && password.trim().length < 6) && <span>This field is must have at least 6 characters</span> }
+
             <input type="password" placeholder="Repeat password" name="password2" value={ password2 } onChange= { handleOnChange }/>
+            { password2.trim().length <= 0 && <span>This field is required</span> }
+            { (password2.trim().length > 0 && password2.trim().length < 6) && <span>This field is must have at least 6 characters</span> }
+            { (password2.trim().length >= 6 && password.trim() !== password2.trim()) && <span>Passwords are not equals.</span> }
+
             <button type="submit">Create</button>
+            <button type="button" onClick={ resetForm }>Reset</button>
         </form>
     </div>
     )
