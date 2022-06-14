190d531d38f707bb1e3fc64b527a30cc8cebdcc6
juan.gomez <gomezj01@stepstone.com>
2022-06-14 10:51:04 -0600

    Dynamic validations.

--- src/pages/DynamicFormPage.tsx
+++ src/pages/DynamicFormPage.tsx
@@ -1,17 +1,36 @@
 import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+
 import { MySelect, MyTextInput } from '../components';
+
 import formJSON from '../data/custom-form.json';
 
-const initialValue:{[key: string]: any} = { };
+const initialValue: {[key: string]: any} = { };
+const requiredFields: {[key:string]: any} = { };
+
 for(const input of formJSON){
-  initialValue[input.name] = input.value
+  initialValue[input.name] = input.value;
+
+  if(!input.validations) continue;
+
+  let schema = Yup.string();
+
+  for (const rule of input.validations) {
+    if(rule.type === 'required') {
+      schema = schema.required(`${input.label} is required`)
+    }
+  }
+
+  requiredFields[input.name] = schema;
 }
+
+const validationsSchema = Yup.object( {...requiredFields} );
 
 export const DynamicFormPage = () => {
   return (
     <div>
       <div>DynamicFormPage</div>
-      <Formik initialValues={ initialValue } onSubmit={ (values) => { console.log(values) } }>
+      <Formik initialValues={ initialValue } onSubmit={ (values) => { console.log(values) } } validationSchema={ validationsSchema }>
         { (formik) => (
           <Form noValidate>
             { formJSON.map( ( { type, label, placeholder, name, options } ) => {
