74d8af72e5827a63567c0a4e1b8294d387736876
juan.gomez <gomezj01@stepstone.com>
2022-06-14 10:29:59 -0600

    Create a select dynamically.

--- src/pages/DynamicFormPage.tsx
+++ src/pages/DynamicFormPage.tsx
@@ -1,5 +1,5 @@
 import { Formik, Form } from 'formik';
-import { MyTextInput } from '../components';
+import { MySelect, MyTextInput } from '../components';
 import formJSON from '../data/custom-form.json';
 
 const initialValue:{[key: string]: any} = { };
@@ -14,8 +14,24 @@ export const DynamicFormPage = () => {
       <Formik initialValues={ initialValue } onSubmit={ (values) => { console.log(values) } }>
         { (formik) => (
           <Form noValidate>
-            { formJSON.map( ( { type, label, placeholder, name } ) => {
-              return <MyTextInput key={ name } type={ (type as any) } label={label} name={name} placeholder={placeholder}/>
+            { formJSON.map( ( { type, label, placeholder, name, options } ) => {
+              if(type === 'input' || type === 'password' || type === 'email')
+                return <MyTextInput key={ name } type={ (type as any) } label={label} name={name} placeholder={placeholder}/>
+
+              if (type === 'select')
+                return (
+                  <MySelect key={ name } label={label} name={name} placeholder={placeholder}>
+                    <option value="">Select an option</option>
+                    {
+                      options?.map( ({id, label}) => (
+                        <option key={id} value={id}>{label}</option>
+                      ))
+                    }
+                  </MySelect>
+                )
+
+
+              throw new Error(`Type: ${type} is not soported`);
              }) }
             <button type='submit'>Submit</button>
           </Form>
